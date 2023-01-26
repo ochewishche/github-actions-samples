b895665c7378a92b12734b884a08a07f6633e75a
ochewishche <>
2023-01-26 10:16:04 +0100

    change added

--- src/test.py
+++ src/test.py
@@ -2,6 +2,6 @@ import spacy
 
 nlp = spacy.load("en_core_web_sm")
 
-doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
+doc = nlp("Apple is looking at buying U.K. startup for $1 billion updated")
 
 print(doc.cats)
